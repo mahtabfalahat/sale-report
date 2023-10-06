bcc075afa2e2d9a01aa5975f42ec92e61a0c8e3c
mahtabfalahat <mahtab.falahatnezhad98@gmail.com>
2023-10-06 22:40:13 +0330

    create custom appbar and drawer

---
+++ src/components/CustomDrawer/CustomDrawer.js
@@ -0,0 +1,63 @@
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Toolbar from '@mui/material/Toolbar';
+
+const CustomDrawer = ({handleDrawerToggle , drawerWidth , mobileOpen  , window}) => {
+    const drawer = (
+        <div>
+            <Toolbar />
+            <Divider />
+            <List>
+                {['All Reports'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <TrendingDownIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+    return (
+        <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders" >
+            <Drawer
+                container={container}
+                variant="temporary"
+                open={mobileOpen}
+                onClose={handleDrawerToggle}
+                ModalProps={{
+                    keepMounted: true,
+                }}
+                sx={{
+                    display: { xs: 'block', sm: 'none' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+            >
+                {drawer}
+            </Drawer>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+                open>
+                {drawer}
+            </Drawer>
+        </Box>
+    )
+}
+
+export default CustomDrawer; 
